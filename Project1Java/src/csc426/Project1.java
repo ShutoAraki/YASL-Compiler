a4c6b018829f23076761663c36c59b3abefffd06
ShutoAraki <shutoaraki_2020@depauw.edu>
2018-09-05 17:59:12 -0400

    Change the input form back

--- Project1Java/src/csc426/Project1.java
+++ Project1Java/src/csc426/Project1.java
@@ -6,28 +6,19 @@ import java.io.InputStreamReader;
 import java.io.FileReader;
 
 /**
- * Main class for Project 1 -- Scanner for a Subset of YASL (Fall 2018). Scans
+ * Main class for Project 1 -- Scanner for a Subset of YASL (Fall 2015). Scans
  * tokens from standard input and prints the token stream to standard output.
  * 
- * @author bhoward and Shuto Araki
+ * @author bhoward
  */
 public class Project1 {
 	public static void main(String[] args) throws IOException {
-		//Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-		
-		try {
-			Scanner scanner = new Scanner(new FileReader("src/test_files/test2.txt"));
-			
-			Token token;
-			do {
-				token = scanner.next();
-				System.out.println(token);
-			} while (token.type != TokenType.EOF);
-
-			scanner.close();
-		} catch (Exception e) {
-			System.err.println(e);
-		}
-
+		Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+ 		Token token;
+		do {
+			token = scanner.next();
+			System.out.println(token);
+		} while (token.type != TokenType.EOF);
+ 		scanner.close();
 	}
 }
